75b044df6b1278085e04cd28179bbea21ae858d5
Jim Cresswell <jim.cresswell@gmail.com>
2022-04-26 12:52:14 +0100

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,16 @@
 module.exports = {
   env: {
-    browser: true,
+    node: true,
     es2021: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  plugins: ["wdio"],
+  extends: ["eslint:recommended", "plugin:wdio/recommended"],
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
   rules: {
+    "max-len": [1, { code: 150 }],
+    quotes: [1, "double"],
   },
 };
